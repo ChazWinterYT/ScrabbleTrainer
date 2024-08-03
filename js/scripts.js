260fa55047b5680f6bea927841e8dda0482dbeff
Chaz Winter <chas@hmrmusic.com>
2024-08-02 20:33:19 -0400

    Built basic layout of game area, with buttons for each game

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ function loadGame(game) {
     playArea.innerHTML = `<p>Loading ${game}...</p>`;
     
     setTimeout(() => {
-        playArea.innerHTML = `<h2>${game}</h2><p>This will be the ${game} training game. The loading times are fake, btw.</p>`;
+        playArea.innerHTML = `
+            <h2>${game}</h2>
+            <p>This will be the ${game} training game.</p>
+            <p class="small-text">(The loading times are fake, btw.)</p>
+        `;
     }, 500); // Simulating a load delay
 }
